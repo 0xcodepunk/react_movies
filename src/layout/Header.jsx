dff116865e34adffabb1e07b8f66f7764d6ad7e8
Crypto_Blinder <1329474@gmail.com>
2025-02-27 21:22:36 +0300

    Проведен рефакторинг проекта на функциональных компонентах

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
     return <nav className="teal accent-3">
         <div className="nav-wrapper">
-        <a href="https://github.com/username/react-movies" className="brand-logo">React Movies</a>
+        <a href="https://github.com/1329474/react-movies" className="brand-logo">React Movies</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="https://github.com/username/react-movies">Repo</a></li>
+            <li><a href="https://github.com/1329474/react-movies">Repo</a></li>
         </ul>
         </div>
     </nav>
